da2bac8ee5b20a44fca634ddf738d997ad933a3f
fermin <ferminarellano.hn@gmail.com>
2016-11-08 15:59:49 -0600

    Created website app; Created base.html with purchased template; Upload static files.

--- naturerich/settings.py
+++ naturerich/settings.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website'
 ]
 
 MIDDLEWARE = [
@@ -28,7 +29,7 @@ ROOT_URLCONF = 'naturerich.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
